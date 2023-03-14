a8ff99b522065d39b715371cea1f01650b5b2ead
Pfpfpfpfal <91882875+Pfpfpfpfal@users.noreply.github.com>
2023-03-14 23:01:45 +0300

    Add files via upload

---
+++ Logarifm/Logarifm/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace Logarifm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_sim = new System.Windows.Forms.Button();
+            this.btn_sqr = new System.Windows.Forms.Button();
+            this.btn_del = new System.Windows.Forms.Button();
+            this.textSim = new System.Windows.Forms.TextBox();
+            this.text_sqr = new System.Windows.Forms.TextBox();
+            this.text_del = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btn_sim
+            // 
+            this.btn_sim.Location = new System.Drawing.Point(348, 179);
+            this.btn_sim.Name = "btn_sim";
+            this.btn_sim.Size = new System.Drawing.Size(75, 23);
+            this.btn_sim.TabIndex = 0;
+            this.btn_sim.Text = "Simpson";
+            this.btn_sim.UseVisualStyleBackColor = true;
+            this.btn_sim.Click += new System.EventHandler(this.btn_sim_Click);
+            // 
+            // btn_sqr
+            // 
+            this.btn_sqr.Location = new System.Drawing.Point(348, 208);
+            this.btn_sqr.Name = "btn_sqr";
+            this.btn_sqr.Size = new System.Drawing.Size(75, 23);
+            this.btn_sqr.TabIndex = 2;
+            this.btn_sqr.Text = "Sqr";
+            this.btn_sqr.UseVisualStyleBackColor = true;
+            this.btn_sqr.Click += new System.EventHandler(this.btn_sqr_Click);
+            // 
+            // btn_del
+            // 
+            this.btn_del.Location = new System.Drawing.Point(348, 237);
+            this.btn_del.Name = "btn_del";
+            this.btn_del.Size = new System.Drawing.Size(75, 23);
+            this.btn_del.TabIndex = 3;
+            this.btn_del.Text = "Del";
+            this.btn_del.UseVisualStyleBackColor = true;
+            this.btn_del.Click += new System.EventHandler(this.btn_del_Click);
+            // 
+            // textSim
+            // 
+            this.textSim.Location = new System.Drawing.Point(429, 179);
+            this.textSim.Name = "textSim";
+            this.textSim.Size = new System.Drawing.Size(100, 20);
+            this.textSim.TabIndex = 4;
+            this.textSim.TextChanged += new System.EventHandler(this.textSim_TextChanged);
+            // 
+            // text_sqr
+            // 
+            this.text_sqr.Location = new System.Drawing.Point(429, 208);
+            this.text_sqr.Name = "text_sqr";
+            this.text_sqr.Size = new System.Drawing.Size(100, 20);
+            this.text_sqr.TabIndex = 5;
+            this.text_sqr.TextChanged += new System.EventHandler(this.text_sqr_TextChanged);
+            // 
+            // text_del
+            // 
+            this.text_del.Location = new System.Drawing.Point(429, 240);
+            this.text_del.Name = "text_del";
+            this.text_del.Size = new System.Drawing.Size(100, 20);
+            this.text_del.TabIndex = 6;
+            this.text_del.TextChanged += new System.EventHandler(this.text_del_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.text_del);
+            this.Controls.Add(this.text_sqr);
+            this.Controls.Add(this.textSim);
+            this.Controls.Add(this.btn_del);
+            this.Controls.Add(this.btn_sqr);
+            this.Controls.Add(this.btn_sim);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_sim;
+        private System.Windows.Forms.Button btn_sqr;
+        private System.Windows.Forms.Button btn_del;
+        private System.Windows.Forms.TextBox textSim;
+        private System.Windows.Forms.TextBox text_sqr;
+        private System.Windows.Forms.TextBox text_del;
+    }
+}
+
---
+++ Logarifm/Logarifm/Form1.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Logarifm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void btn_sim_Click(object sender, EventArgs e)
+        {/*
+            class Program
+        {
+            
+            private static double Simpson(Func<double, double> f, double a, double b, int n)
+            {
+                var h = (b - a) / n;
+                var sum1 = 0d;
+                var sum2 = 0d;
+                for (var k = 1; k <= n; k++)
+                {
+                    var xk = a + k * h;
+                    if (k <= n - 1)
+                    {
+                        sum1 += f(xk);
+                    }
+
+                    var xk_1 = a + (k - 1) * h;
+                    sum2 += f((xk + xk_1) / 2);
+                }
+
+                var result = h / 3d * (1d / 2d * f(a) + sum1 + 2 * sum2 + 1d / 2d * f(b));
+                return result;
+            }
+
+            static void Main(string[] args)
+            {
+                
+                double f(double x) => x / (x - 1);
+
+                var result = Simpson(f, 4, 10, 1000);
+                Console.WriteLine("S = {0}", result);
+
+                Console.ReadKey();
+            }
+        }*/
+        }
+
+        private void btn_del_Click(object sender, EventArgs e)
+        {
+            /*
+class Program
+{
+    
+    static double Abs(double x)
+    {
+        return (x < 0) ? -x : x;
+    }
+
+    
+    static int Sign(double x)
+    {
+        return (x < 0) ? -1 : 1;
+    }
+
+    
+    static double BisectionMethod(Func<double, double> function, double a, double b, double precision = 1e-10)
+    {
+        while (true)
+        {
+            var t = (a + b) / 2;
+
+            if (function(t) == 0.0 || Abs(b - a) < Abs(precision))
+            {
+                return t;
+            }
+
+            if (Sign(function(t)) == Sign(function(a)))
+            {
+                a = t;
+            }
+            else
+            {
+                b = t;
+            }
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        
+        double f(double x) => 3 * Math.Pow(x, 2) - 6 * x + 1;
+
+        Console.WriteLine("3x^2 - 6x + 1");
+        Console.Write("Введите начало отрезка: ");
+        var a = double.Parse(Console.ReadLine());
+        Console.Write("Введите конец отрезка: ");
+        var b = double.Parse(Console.ReadLine());
+        Console.Write("Введите точность вычислений: ");
+        var eps = double.Parse(Console.ReadLine());
+
+        var result = BisectionMethod(f, a, b, eps);
+        Console.WriteLine("x = {0}\r\nf({0}) = {1}", result, f(result));
+        Console.ReadKey();
+    }
+}*/
+        }
+
+        private void btn_sqr_Click(object sender, EventArgs e)
+        {
+            /*class Program
+{
+    static double LeftRectangle(Func<double, double> f, double a, double b, int n)
+    {
+        var h = (b - a) / n;
+        var sum = 0d;
+        for(var i = 0; i <= n-1; i++)
+        {
+            var x = a + i * h;
+            sum += f(x);
+        }
+
+        var result = h * sum;
+        return result;
+    }
+
+    static double RightRectangle(Func<double, double> f, double a, double b, int n)
+    {
+        var h = (b - a) / n;
+        var sum = 0d;
+        for (var i = 1; i <= n; i++)
+        {
+            var x = a + i * h;
+            sum += f(x);
+        }
+
+        var result = h * sum;
+        return result;
+    }
+
+    static double CentralRectangle(Func<double, double> f, double a, double b, int n)
+    {
+        var h = (b - a) / n;
+        var sum = (f(a) + f(b)) / 2;
+        for (var i = 1; i < n; i++)
+        {
+            var x = a + h * i;
+            sum += f(x);
+        }
+
+        var result = h * sum;
+        return result;
+    }
+
+    static void Main(string[] args)
+    {
+        
+        double f(double x) => x / (x - 1);
+
+        var result = LeftRectangle(f, 4, 10, 1000);
+        Console.WriteLine("Формула левых прямоугольников: {0}", result);
+        result = RightRectangle(f, 4, 10, 1000);
+        Console.WriteLine("Формула правых прямоугольников: {0}", result);
+        result = CentralRectangle(f, 4, 10, 1000);
+        Console.WriteLine("Формула средних прямоугольников: {0}", result);
+
+        Console.ReadKey();
+    }
+}*/
+        }
+
+        private void textSim_TextChanged(object sender, EventArgs e)
+        {
+            /**/
+        }
+
+        private void text_sqr_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void text_del_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
